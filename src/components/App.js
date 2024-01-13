382b514d870c809c5d4aba8b927a71c2c3deaa08
UtsavKumar126/ <guptautsavkumar126@gmail.com>
2024-01-13 21:26:50 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
+  const[fruitsArr,setFruitsArr]=useState(fruits);
+  function findFruits(e){
+    if(e==="")setFruitsArr(fruits);
+    setFruitsArr(fruitsArr.filter(ele=>ele.indexOf(e)>-1));
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <input placeholder="Enter Fruit" onChange={(e)=>findFruits(e.target.value)}/>
+        <ul>
+          {
+            fruitsArr.map((ele)=>
+            <li key={ele}>{ele}</li>
+            )
+          }
+        </ul>
     </div>
   )
 }
